bf26999c2416f9eef5f26211bf4b922ffe0b4324
ManviPrakash <prakashmanvi33@gmail.com>
2025-01-19 21:20:07 -0500

    Add code to lab1.py

---
+++ Week01/lab1.py
@@ -0,0 +1,15 @@
+ # Sample coding questions "01 Week01".
+ # Manvi Prakash
+array = [1, 4, 7, 9]
+
+a, b, c, d = 1, 2, 3, 4
+e = (a * c) - (b / d)
+e_full = ((a - (b ** c)) // d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius.".format(temperature))
+
+
+user_age = int(input("Enter your age: "))
+user_age += 22
+print("Now showing the shop items filtered by age:",user_age)
